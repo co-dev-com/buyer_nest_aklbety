81a955033b28419f03fc34d843a6924d4399e1f7
Ahmed Yousry <97663058+ahmedyousry0098@users.noreply.github.com>
2024-06-19 11:57:07 +0300

    app config (#1)
    * app config
    
    * remove prisma migration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({logger: true})
+  );
+  const configService = app.get(ConfigService)
+  const port = configService.get('port');
+  await app.listen(port);
 }
 bootstrap();
