3b56b7851062906226f7c21a6d69aec17da0a916
ahmedyousry0098 <ahmedyousry098@gmail.com>
2024-07-14 12:40:42 +0300

    use nest express

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({logger: true})
-  );
-  const configService = app.get(ConfigService)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
   const port = configService.get('port');
   await app.listen(port);
 }
